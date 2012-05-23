1ad7eff581390995da9109a274a3c83778fe3eaa
Christopher <chrissitopher@gmx.de>
2012-05-23 22:30:44 +0200

    [TASK] Migrate doc_tut_quickstart 0.1.0
    Import doc_tut_quickstart 0.1.0.
    
    Change-Id: Ic8dc260fcb0e765b4417455b234a66e43ffb35b6

--- doc_tut_quickstart/ext_emconf.php
+++ doc_tut_quickstart/ext_emconf.php
@@ -1,18 +1,18 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "doc_tut_quickstart".
+# Extension Manager/Repository config file for ext: "doc_tut_quickstart"
 #
-# Auto generated 29-12-2003 16:11
+# Auto generated 11-02-2010 19:59
 #
 # Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Getting Started',
-	'description' => 'Gets you started hands on with your first TYPO3 website. Will explain the basic principles of TYPO3 and gives you hands on experience. After this tutorial you will have idea of the capabilities of TYPO3 as well as complexities involved.',
+	'title' => 'doc_tut_quickstart',
+	'description' => '',
 	'category' => 'doc',
 	'shy' => 0,
 	'dependencies' => '',
@@ -28,10 +28,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'lockType' => '',
 	'author' => 'Kasper Skårhøj',
 	'author_email' => 'kasper@typo3.com',
-	'author_company' => 'Curby Soft Multimedia',
+	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '0.0.4',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '3.5.0-0.0.0',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:1:{s:14:"doc/manual.sxw";s:4:"1eae";}',
+	'_md5_values_when_last_written' => 'a:1:{s:14:"doc/manual.sxw";s:4:"7dfa";}',
 );
 
 ?>
