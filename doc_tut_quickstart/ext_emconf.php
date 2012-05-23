99f059b714433756d23cf464bb8e7ec3c6385eca
Christopher <chrissitopher@gmx.de>
2012-05-23 22:32:33 +0200

    [TASK] Migrate doc_tut_quickstart 1.0.0
    Import doc_tut_quickstart 1.0.0.
    
    Change-Id: I2135e83caae19642981a81e6ae81165e8b8545a0

--- doc_tut_quickstart/ext_emconf.php
+++ doc_tut_quickstart/ext_emconf.php
@@ -1,20 +1,21 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "doc_tut_quickstart"
+# Extension Manager/Repository config file for ext "doc_tut_quickstart".
 #
-# Auto generated 11-02-2010 19:59
+# Auto generated 08-12-2010 16:15
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'doc_tut_quickstart',
-	'description' => '',
+	'title' => 'Getting Started tutorial',
+	'description' => 'A tutorial to discover the main features and concepts of TYPO3. It is based on the Introduction Package.',
 	'category' => 'doc',
 	'shy' => 0,
+	'version' => '1.0.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -24,25 +25,23 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
-	'author' => 'Kasper Skårhøj',
-	'author_email' => 'kasper@typo3.com',
+	'author' => 'Official Documentation',
+	'author_email' => 'documentation@typo3.org',
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '3.5.0-0.0.0',
-			'php' => '3.0.0-0.0.0',
+			'typo3' => '4.4.0-4.4.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:1:{s:14:"doc/manual.sxw";s:4:"7dfa";}',
+	'_md5_values_when_last_written' => 'a:3:{s:9:"ChangeLog";s:4:"6122";s:12:"ext_icon.gif";s:4:"7277";s:14:"doc/manual.sxw";s:4:"243a";}',
 );
 
 ?>
