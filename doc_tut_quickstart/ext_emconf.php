0a9b200acf58252ee5a6eaf71a65246e38343d43
Christopher <chrissitopher@gmx.de>
2012-05-23 22:28:55 +0200

    [TASK] Migrate doc_tut_quickstart 0.0.4
    Import doc_tut_quickstart 0.0.4.
    
    Change-Id: Ied943de95098988a3d6872819f3b2abebb2925ea

---
+++ doc_tut_quickstart/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "doc_tut_quickstart".
+#
+# Auto generated 29-12-2003 16:11
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Getting Started',
+	'description' => 'Gets you started hands on with your first TYPO3 website. Will explain the basic principles of TYPO3 and gives you hands on experience. After this tutorial you will have idea of the capabilities of TYPO3 as well as complexities involved.',
+	'category' => 'doc',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Kasper Skårhøj',
+	'author_email' => 'kasper@typo3.com',
+	'author_company' => 'Curby Soft Multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '0.0.4',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '3.5.0-0.0.0',
+			'php' => '3.0.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:1:{s:14:"doc/manual.sxw";s:4:"1eae";}',
+);
+
+?>
