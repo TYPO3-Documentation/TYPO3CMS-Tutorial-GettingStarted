e2f4dccfbe81993f477b837c0ce2d41ddee11cc8
Christopher <chrissitopher@gmx.de>
2012-05-23 23:12:53 +0200

    [TASK] Migrate doc_tut_quickstart_da master
    Import doc_tut_quickstart_da, last version from SVN.
    
    Change-Id: Iae66625bb70ab06c15a3f9508056e4e1578a80bf

---
+++ doc_tut_quickstart_da/ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "doc_tut_quickstart".
+#
+# Auto generated 08-12-2010 12:21
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Getting Started tutorial',
+	'description' => 'A tutorial to discover the main features and concepts of TYPO3. It is based on the Introduction Package.',
+	'category' => 'doc',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'doNotLoadInFE' => 1,
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Official Documentation',
+	'author_email' => 'documentation@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.4.0-4.4.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"6122";s:12:"ext_icon.gif";s:4:"7277";s:26:"doc/doc_tut_quickstart.pdf";s:4:"1f62";s:26:"doc/doc_tut_quickstart.txt";s:4:"9677";s:14:"doc/manual.sxw";s:4:"243a";}',
+	'suggests' => array(
+	),
+);
+
+?>
