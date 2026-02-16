3cbb2f98c694ce4b72e559fc4e32e60ea0c869e3
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2026-02-16 11:52:50 +0100

    [FEATURE] Add Quickstart Chapter (#779)
    * [FEATURE] Add Quickstart Chapter
    
    Releases: main
    
    * [TASK] Added test to increase clarity
    
    Releases: main
    
    * Apply suggestion from @sarahmccarthy123
    
    Co-authored-by: Sarah McCarthy <sarahmccarthy123@yahoo.com>
    
    * Apply suggestion from @linawolf
    
    ---------
    
    Co-authored-by: Sarah McCarthy <sarahmccarthy123@yahoo.com>

---
+++ Documentation/Quickstart/_copy_theme_composer.sh
@@ -0,0 +1,2 @@
+cp -R vendor/typo3/theme-camino/ packages/theme-camino
+ddev composer req typo3/theme-camino:"@dev"
---
+++ Documentation/Quickstart/_extend_theme_composer.sh
@@ -0,0 +1,19 @@
+ddev composer req friendsoftypo3/kickstarter
+
+ddev typo3 make:extension
+# [...]
+# Composer package name:
+#  > myvendor/theme_pluto
+
+Keep the default values for all other questions.
+
+ddev typo3 make:site-set
+
+# Please enter the site set identifier (must be "myvendor/theme_pluto" or "myvendor/theme_pluto-<suffix>") [myvendor/theme_pluto]:
+#  >
+# [...]
+#
+# Enter site set identifiers for dependencies (press Enter with empty input to finish).
+#
+#  Dependency (vendor/package):
+#  > typo3/theme-camino
